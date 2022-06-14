ca4e38a7d2b175a98c8b38755852b0bea0d4917c
still_Jerry <oljaapelsin@gmail.com>
2022-06-14 14:13:42 +0300

    Replace change from OrderView to CheckoutView

--- Dinner/CheckoutView.swift
+++ Dinner/CheckoutView.swift
@@ -12,8 +12,10 @@ struct CheckoutView: View {
     @State private var paymentType = "Cash"
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
     var body: some View {
         let paymentTypes = ["Cash", "Credit Card", "Dinner Points"]
+        let tipAmounts = [10, 15, 20, 25, 0]
 
         Form {
             Section {
@@ -28,12 +30,42 @@ struct CheckoutView: View {
                     }
                 }
             }
+            Section {
+                NavigationLink(destination: CheckoutView()) {
+
+                    Text("Place Order")
+                }
+            }
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header:
+                Text("TOTAL: \(totalPrice)")
+            ) {
+                Button("Confirm order") {
+                    // place the order
+                }
+            }
             
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
        
 
+    }
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+
+        return formatter.string(from: NSNumber(value: total + tipValue)) ?? "$0"
     }
 }
 
--- Dinner/OrderView.swift
+++ Dinner/OrderView.swift
@@ -9,11 +9,10 @@ import SwiftUI
 
 struct OrderView: View {
     @EnvironmentObject var order: Order
-    @State private var tipAmount = 15
+    
     var body: some View {
             NavigationView {
                 List {
-                    let tipAmounts = [10, 15, 20, 25, 0]
 
                     Section {
                         ForEach(order.items) { item in
@@ -25,27 +24,7 @@ struct OrderView: View {
                         }
                     }
 
-                    Section {
-                        NavigationLink(destination: CheckoutView()) {
-
-                            Text("Place Order")
-                        }
-                    }
-                    Section(header: Text("Add a tip?")) {
-                        Picker("Percentage:", selection: $tipAmount) {
-                            ForEach(tipAmounts, id: \.self) {
-                                Text("\($0)%")
-                            }
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                    }
-                    Section(header:
-                        Text("TOTAL: $100")
-                    ) {
-                        Button("Confirm order") {
-                            // place the order
-                        }
-                    }
+                   
                 }
                 .navigationTitle("Order")
                 .listStyle(InsetGroupedListStyle())
