03f251b25c3d23b004c819a5c61e9c65317b53c6
still_Jerry <oljaapelsin@gmail.com>
2022-06-14 13:15:39 +0300

    append in Order

--- Dinner/DinnerApp.swift
+++ Dinner/DinnerApp.swift
@@ -12,7 +12,7 @@ struct DinnerApp: App {
     @StateObject var order = Order()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environmentObject(order)
         }
     }
--- Dinner/MainView.swift
+++ Dinner/MainView.swift
@@ -25,6 +25,7 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        MainView().environmentObject(Order())
+
     }
 }
