a787e8383f19948ffa18eac64a4778dc32fe2f4d
still_Jerry <oljaapelsin@gmail.com>
2022-06-14 11:46:13 +0300

    Initial Commit

---
+++ Dinner/DinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinnerApp.swift
+//  Dinner
+//
+//  Created by Anatolich Mixaill on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct DinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
